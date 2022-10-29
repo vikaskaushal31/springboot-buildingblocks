a733330bb646deb2160fbb75fb99e1a72fbe0042
Vikas Kaushal <vikaskaushal31@gmail.com>
2022-10-29 14:33:26 +0530

    Hello World Base

---
+++ src/main/java/com/stacksimplify/restservices/Hello/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.stacksimplify.restservices.Hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloWorldController {
+
+	//Simple Method
+	//@RequestMapping(method = RequestMethod.GET, path = "/helloworld")
+	@GetMapping("/helloworld1")
+	public String helloWorld() {
+		return "Hello World1";
+	}
+	
+	@GetMapping("/helloworld-bean")
+	public UserDetails helloWorldBean() {
+		return new UserDetails("Kalyan", "Reddy", "Hyderabad");
+	}
+}
