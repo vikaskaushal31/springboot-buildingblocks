c4620ebbf2b6732f046ee3956047a32df85cfe5a
Vikas Kaushal <vikaskaushal31@gmail.com>
2022-10-31 22:30:06 +0530

    Exception commit

---
+++ src/main/java/com/stacksimplify/restservices/exceptions/UserExistsException.java
@@ -0,0 +1,15 @@
+package com.stacksimplify.restservices.exceptions;
+
+public class UserExistsException extends Exception {
+
+	
+	private static final long serialVersionUID = 8966650283572035376L;
+
+
+	public UserExistsException(String message) {
+		super(message);
+	}
+	
+	
+
+}
