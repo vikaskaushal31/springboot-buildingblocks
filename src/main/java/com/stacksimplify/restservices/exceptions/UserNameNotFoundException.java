0c1212f148f8b7e64739da1d64a39ef68a83a7aa
Vikas Kaushal <vikaskaushal31@gmail.com>
2022-11-01 23:34:45 +0530

    untracked file commit

---
+++ src/main/java/com/stacksimplify/restservices/exceptions/UserNameNotFoundException.java
@@ -0,0 +1,17 @@
+package com.stacksimplify.restservices.exceptions;
+
+public class UserNameNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public UserNameNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+}
